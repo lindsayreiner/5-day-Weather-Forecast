dc331d48661d015d7230c1073760ce3d9adb0acb
Lindsay Cristina Reiner <lindsayreiner@hey.com>
2021-09-12 20:09:25 -0400

    API weather data displaying for todays forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,22 @@ var APIKey = "770f31b5bb9e29f326535d8549df41b6";
 
 //User input variables
 
-
 var cityList = $("#cities");
 var cityUserInput = $("#city-input");
 var cities = [];
+
+//Today weather forecast card variables
+
+var todayCity = $('#today-city');
+var todayDate = $('#today-date');
+var todayTemp = $('#today-temp');
+var todayWind = $('#today-wind');
+var todayHumidity = $('#today-humidity');
+var todayDescription = $('#today-description');
+var todayIcon = $('#weather-icon');
+
+//Five day forecast card variables
+
 
 
 
@@ -26,34 +38,64 @@ $('#search-btn').on('click', async function (e) {
     const data = await response.json()
         .then(data => {
             var nameResponse = data['name'];
+            var dateResponse = data['']
             var tempResponse = data['main']['temp'];
             var windResponse = data['wind']['speed'];
             var humidityResponse = data['main']['humidity'];
-            var cityLatitude = data['coord']['lat']
-            var cityLongitude = data['coord']['lon']
+            var cityLatitude = data['coord']['lat'];
+            var cityLongitude = data['coord']['lon'];
+            var weatherDescription = data['weather'][0]['description'];
+            var weatherIcon = data['weather'][0]['main'];
+
+            console.log(nameResponse);
             console.log(tempResponse);
-            console.log(windResponse);
             console.log(humidityResponse);
-            console.log(cityLatitude);
-            console.log(cityLongitude);
+            console.log(weatherDescription);
+            console.log(windResponse);
             console.log(data);
+
+
+            todayCity.text(nameResponse);
+            todayDescription.text('Forecast: ' + weatherDescription);
+            todayTemp.text('Temp: ' + tempResponse + ' F');
+            todayWind.text('Wind (mph): ' + windResponse + ' m.p.h.');
+            todayHumidity.text('Humidity: ' + humidityResponse + '%');
+
+
+            if (weatherDescription === 'sunny') {
+                todayIcon.text('<i class="fas fa-sun"></i>');
+            }
+
+            if (weatherDescription === 'clouds') {
+                todayIcon.text('<i class="fas fa-cloud-sun"></i>');
+            }
+
+            if (weatherDescription === 'rain') {
+                todayIcon.text('<i class="fas fa-cloud-rain"></i>');
+            }
+
+            if (weatherDescription === 'wind') {
+                todayIcon.text('<i class="fas fa-wind"></i>');
+            }
+
 
         })
 
     addCity(city);
     renderCities();
-    secondAPICall();
+    // secondAPICall();
 
 });
 
-function secondAPICall() {
+// function secondAPICall() {
 
-    // var uvIndexAPICall = `https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}`
-    // fetch(uvIndexAPICall);
-}
+//     var uvIndexAPICall = `https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}`
+//     fetch(uvIndexAPICall);
+// }
 
 $(".city-button").on('click', function (e) {
     e.preventDefault();
+    // firstAPICall();
 
 
 })

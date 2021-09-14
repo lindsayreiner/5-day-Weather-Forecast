50254880c94fc1da3bff031b42a09174789ff00b
Lindsay Cristina Reiner <lindsayreiner@hey.com>
2021-09-13 21:56:24 -0400

    fixed city buttons, added second API call, all functional except 5 day weather forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,7 +15,7 @@ var todayTemp = $('#today-temp');
 var todayWind = $('#today-wind');
 var todayHumidity = $('#today-humidity');
 var todayDescription = $('#today-description');
-var todayIcon = $('#weather-icon');
+var todayUVIndex = $('#today-uv-index')
 
 //Current weather and future weather containers
 
@@ -37,12 +37,13 @@ $('#search-btn').on('click', function (e) {
 });
 
 var cityBtn = $('.city-button')
-$('#cities').on('click', 'button', function (e, city) {
+$('#cities').on('click', 'button', function (e) {
     e.preventDefault();
-    console.log(e)
 
-    console.log(city)
-    firstAPICall(city);
+    todayWeather.removeClass('not-visible')
+    futureWeather.removeClass('hidden')
+
+    firstAPICall($(this)[0].innerHTML);
 });
 
 var clearBtn = $('#clear-btn');
@@ -52,73 +53,92 @@ clearBtn.on('click', function () {
 })
 
 
+
 async function firstAPICall(city) {
 
 
     var cityQueryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${APIKey}`;
 
-
     const response = await fetch(cityQueryURL);
 
     const data = await response.json()
-        .then(data => {
-            var nameResponse = data['name'];
-            var dateResponse = data['']
-            var tempResponse = data['main']['temp'];
-            var windResponse = data['wind']['speed'];
-            var humidityResponse = data['main']['humidity'];
-            var cityLatitude = data['coord']['lat'];
-            var cityLongitude = data['coord']['lon'];
-            var weatherDescription = data['weather'][0]['description'];
-            var weatherIcon = data['weather'][0]['main'];
+    console.log(data)
+
+    var nameResponse = data['name'];
+    var dateResponse = data['']
+    var tempResponse = data['main']['temp'];
+    var windResponse = data['wind']['speed'];
+    var humidityResponse = data['main']['humidity'];
+    var cityLat = data['coord']['lat'];
+    var cityLon = data['coord']['lon'];
+    var weatherDescription = data['weather'][0]['description'];
+    var weatherIcon = data['weather'][0]['icon'];
 
 
-            todayCity.text(nameResponse);
-            todayDescription.text('Forecast: ' + weatherDescription);
-            todayTemp.text('Temp: ' + tempResponse + ' F');
-            todayWind.text('Wind (mph): ' + windResponse + ' m.p.h.');
-            todayHumidity.text('Humidity: ' + humidityResponse + '%');
+    todayCity.text(nameResponse);
+    todayDescription.html('<strong>Forecast:&nbsp</strong>' + weatherDescription);
+    todayTemp.html('<strong>Temp:&nbsp</strong>' + tempResponse + ' F');
+    todayWind.html('<strong>Wind:&nbsp</strong> ' + windResponse + ' m.p.h.');
+    todayHumidity.html('<strong>Humidity:&nbsp</strong> ' + humidityResponse + '%');
+
+    var todayIcon = $('<img>').attr('src', `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`);
+    todayIcon.addClass('icon-style');
+    todayCity.append(todayIcon);
 
 
-            if (weatherDescription === 'sunny') {
-                todayIcon.text('<i class="fas fa-sun"></i>');
-            }
-
-            if (weatherDescription === 'clouds') {
-                todayIcon.text('<i class="fas fa-cloud-sun"></i>');
-            }
-
-            if (weatherDescription === 'rain') {
-                todayIcon.text('<i class="fas fa-cloud-rain"></i>');
-            }
-
-            if (weatherDescription === 'wind') {
-                todayIcon.text('<i class="fas fa-wind"></i>');
-            }
 
 
-        })
 
-    addCity(city);
+
+    addCity(nameResponse);
     renderCities();
-    // secondAPICall();
+    secondAPICall(cityLat, cityLon);
 
 };
 
-// async function secondAPICall(cityLatitude, cityLongitude) {
+async function secondAPICall(cityLat, cityLon) {
 
-//     var uvIndexAPICall = `https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}`
-//     fetch(uvIndexAPICall);
-// }
+    var uvIndexAPICall = `https://api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLon}&units=imperial&appid=${APIKey}`
+
+    const response = await fetch(uvIndexAPICall);
+
+    const data = await response.json();
+
+    console.log(data);
+
+    var uvIndexResponse = data['current']['uvi'];
+
+    var uvSpan = $('<span>').text(uvIndexResponse);
+
+    todayUVIndex.html('<strong>UV-Index:&nbsp</strong>');
+    todayUVIndex.append(uvSpan);
+
+
+    if (uvIndexResponse >= 8) {
+        uvSpan.addClass('uv-extreme-zone');
+        console.log('extreme')
+    } else if (uvIndexResponse < 8 && uvIndexResponse >= 6) {
+        uvSpan.addClass('uv-red-zone');
+        console.log('red')
+    } else if (uvIndexResponse < 6 && uvIndexResponse >= 3) {
+        uvSpan.addClass('uv-yellow-zone');
+        console.log('yellow')
+    } else {
+        uvSpan.addClass('uv-green-zone');
+        console.log('green')
+    }
+
+}
 
 
 
 function addCity(city) {
-    var cityText = cityUserInput.value;
-
-    console.log(cityText);
 
     cities = getCities();
+
+    if (cities.includes(city)) {
+        return;
+    }
 
     cities.push(city);
     cityUserInput.text('');
